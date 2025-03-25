61260a117b6ddda6dcb9eb95d3fd7b108497885d
逸墨 <2798156978@qq.com>
2025-03-25 16:52:09 +0800

    更新api函数调用

--- netlify/functions/direct-test/index.js
+++ netlify/functions/direct-test/index.js
@@ -1,1 +1,13 @@
- 
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Test function working",
+      env: {
+        hasApiKey: !!process.env.VITE_DEEPSEEK_API_KEY,
+        netlifyEnv: process.env.NODE_ENV || 'not set',
+        contextEnv: context.clientContext ? 'present' : 'absent'
+      }
+    })
+  };
+}; 
