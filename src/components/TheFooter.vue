f1406dd0cffa4fea13622d750e3dadd2af790c01
逸墨 <2798156978@qq.com>
2025-04-07 17:00:33 +0800

    添加百度统计

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,11 @@
+// 删除或注释掉以下代码
+// const getVisitorCount = async () => {
+//   try {
+//     const response = await fetch('/.netlify/functions/visitor')
+//     if (!response.ok) throw new Error('Network response was not ok')
+//     const data = await response.json()
+//     pv.value = data.count
+//   } catch (error) {
+//     console.error('获取访问量失败：', error)
+//   }
+// }
